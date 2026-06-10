f394342e836d7a1e5210ed373b3d43570b5c21dd
AleLP4 <alelopezpalma@gmail.com>
2026-06-10 00:43:59 -0600

    RequestController y resolucion de conflictos

--- SRIE/src/main/java/proyectoInfraestructura/SRIE/controller/RequestController.java
+++ SRIE/src/main/java/proyectoInfraestructura/SRIE/controller/RequestController.java
@@ -1,11 +1,61 @@
 package proyectoInfraestructura.SRIE.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+import proyectoInfraestructura.SRIE.model.Request;
+import proyectoInfraestructura.SRIE.model.dto.RequestDTO;
+import proyectoInfraestructura.SRIE.service.RequestService;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/request")
 @RestController
 public class RequestController {
 
+    @Autowired
+    private RequestService service;
 
+    @GetMapping("/all")
+    public ResponseEntity<?> showAll() {
+
+        List<Request> requests = service.findAll();
+
+        if (requests == null || requests.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(requests);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> add(@Valid @RequestBody RequestDTO request, BindingResult result) {
+        if (result.hasErrors()) {
+            List<String> errors = new ArrayList<>();
+
+            for (ObjectError error : result.getAllErrors()) {
+                errors.add(error.getDefaultMessage());
+            }
+
+            return ResponseEntity.badRequest().body(errors);
+        }
+
+        Request newRequest = service.add(request);
+
+        if (newRequest == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No se pudo registrar la solicitud");
+        }
+
+        return ResponseEntity.ok("Solicitud registrada exitosamente");
+    }
 
 
 }
+
+
+
